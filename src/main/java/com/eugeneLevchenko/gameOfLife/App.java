284d04ff3e89f3c3245f0066687073096d473b7c
Eugene Levchenko <eugene140823@gmail.com>
2019-09-19 16:00:41 +0300

    Initialize and prepare input data. Generate random 2D array if there is'nt in input file

--- src/main/java/com/eugeneLevchenko/gameOfLife/App.java
+++ src/main/java/com/eugeneLevchenko/gameOfLife/App.java
@@ -3,7 +3,9 @@ package com.eugeneLevchenko.gameOfLife;
 import java.io.IOException;
 import java.util.Arrays;
 
+import static com.eugeneLevchenko.gameOfLife.SimulateLife.COLS;
 import static com.eugeneLevchenko.gameOfLife.SimulateLife.INPUT_ARR;
+import static com.eugeneLevchenko.gameOfLife.SimulateLife.ROWS;
 
 public class App
 {
