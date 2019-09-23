8ff017628727b81c9fc72fc35bbc85be0300281e
Eugene Levchenko <eugene140823@gmail.com>
2019-09-23 15:07:57 +0300

    Add unit tests and some refactor.

--- src/main/java/com/eugeneLevchenko/gameOfLife/App.java
+++ src/main/java/com/eugeneLevchenko/gameOfLife/App.java
@@ -9,6 +9,6 @@ public class App
 {
     public static void main( String[] args ) throws IOException {
         GameOfLife game = new GameOfLife();
-        //game.startSimulate(false);
+        game.startSimulate(false);
     }
 }
